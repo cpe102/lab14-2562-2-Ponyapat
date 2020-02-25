b6fcc86460ead34a2438f1d54176e53b9d9177ba
Ponyapat <42263205+Ponyapat@users.noreply.github.com>
2020-02-25 16:18:50 +0700

    Take Me  Home

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,10 +1,32 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 
 int main(){
 	//Write your code here.
+	int a = 5;
+	char b = 'A';
+	char &c=b;
+	int *x = &a;
+	char *y = &b;
+	int **z = &x;
 	
+	cout << "a = " << a <<endl;
+	cout << "b = " << b <<endl;
+	cout << "c = " << c <<endl;
+	cout << "x = " << x <<endl;
+	cout << "y = " << (int *)y <<endl;
+	cout << "z = " << z <<endl;
+
+	cout << "address a = " << &a <<endl;
+	cout << "address b = " << (int *)&b <<endl;
+	cout << "address c = " << (int *)&c <<endl;
+	cout << "address x = " << &x <<endl;
+	cout << "address y = " << &y <<endl;
+	cout << "address z = " << &z <<endl;
+
+	
+
 	
 	
 	return 0;
